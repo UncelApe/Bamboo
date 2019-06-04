86096b901e31017db6bf66596bb1df3a7b5dd364
masterUNG <master@abc.com>
2019-06-04 01:59:22 -0700

    Arrow Ok123

--- app/src/main/java/bamboo/boobam/RegisterFragment.java
+++ app/src/main/java/bamboo/boobam/RegisterFragment.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -34,6 +40,67 @@ public class RegisterFragment extends Fragment {
 
     }   //Main method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        if (item.getItemId() == R.id.itemUplod) {
+
+            uploadProcess();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadProcess() {
+
+//        Get Value From Editext
+        EditText nameEditText = getView().findViewById(R.id.editName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passEditText = getView().findViewById(R.id.edtPasword);
+
+//        Convert Edittext to String
+        String name = nameEditText.getText().toString().trim();
+        String user = userEditText.getText().toString().trim();
+        String pass = passEditText.getText().toString().trim();
+
+//        Check Space
+
+        if (name.isEmpty() || user.isEmpty() || pass.isEmpty()) {
+//            Have Space
+            alert("Have Space");
+        } else {
+//            No Space
+
+            try {
+
+                String urlPHP = "http://androidthai.in.th/fff/addDatoApe.php";
+                AddDataThread addDataThread = new AddDataThread(getActivity());
+                addDataThread.execute(name, user, pass, urlPHP);
+                String result = addDataThread.get();
+                Log.d("4JuneV1", "result =" + result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+        }   //if
+    }   // uploadProcess
+
+    private void alert(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_grgister, menu);
+
+
+
+    }
+
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
 
@@ -53,6 +120,7 @@ public class RegisterFragment extends Fragment {
 
             }
         });
+        setHasOptionsMenu(true);
     }
 
     @Override
